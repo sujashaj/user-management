18e366d64b775e5e03be511a76f908112b204ae9
Suja Shaj <sujashaj98@gmail.com>
2023-08-22 16:51:49 -0700

    Added code commits User class

--- user_management/User.py
+++ user_management/User.py
@@ -17,6 +17,19 @@ class User:
  def __init__(self, username, email, password):
     self.username = username
     self.email = email
+    self.password = self._hash_password(password)
+    self.is_verified = False
+
+ def _hash_password(self, password):
+     # Use a secure hashing algorithm like bcrypt or Argon2 in production
+     return hashlib.sha256(password.encode()).hexdigest()
+
+ def verify_password(self, password):
+     hashed_password = self._hash_password(password)
+     return self.password == hashed_password
+
+ def set_verified(self):
+     self.is_verified = True
 
 
 
