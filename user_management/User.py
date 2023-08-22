fc7124d2c83be1bbb774502b117652fa418223f9
Suja Shaj <sujashaj98@gmail.com>
2023-08-22 16:41:18 -0700

    Created ORM set up forrelational table in User.py

--- user_management/User.py
+++ user_management/User.py
@@ -1,18 +1,22 @@
 import hashlib
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'User'
+
+username = Column(String, primary_key=True)
+email = Column(String, unique=True)
+password = Column(String)
+is_verified = Column(Boolean, default=False)
+
 class User:
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = self._hash_password(password)
-        self.is_verified = False
+ def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
 
-    def _hash_password(self, password):
-        # Use a secure hashing algorithm like bcrypt or Argon2 in production
-        return hashlib.sha256(password.encode()).hexdigest()
 
-    def verify_password(self, password):
-        hashed_password = self._hash_password(password)
-        return self.password == hashed_password
 
-    def set_verified(self):
-        self.is_verified = True
