39b41da45f60d2487b41beb9fa545b8efca5662b
Suja Shaj <sujashaj98@gmail.com>
2023-09-02 13:43:39 -0700

    added user registration feature

--- user_management/User.py
+++ user_management/User.py
@@ -8,28 +8,27 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
 
-username = Column(String, primary_key=True)
-email = Column(String, unique=True)
-password = Column(String)
-is_verified = Column(Boolean, default=False)
+    username = Column(String, primary_key=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    is_verified = Column(Boolean, default=False)
 
-class User:
- def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.password = self._hash_password(password)
-    self.is_verified = False
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = self._hash_password(password)
+        self.is_verified = False
 
- def _hash_password(self, password):
-     # Use a secure hashing algorithm like bcrypt or Argon2 in production
-     return hashlib.sha256(password.encode()).hexdigest()
+    def _hash_password(self, password):
+        # Use a secure hashing algorithm like bcrypt or Argon2 in production
+         return hashlib.sha256(password.encode()).hexdigest()
 
- def verify_password(self, password):
-     hashed_password = self._hash_password(password)
-     return self.password == hashed_password
+    def verify_password(self, password):
+        hashed_password = self._hash_password(password)
+        return self.password == hashed_password
 
- def set_verified(self):
-     self.is_verified = True
+    def set_verified(self):
+        self.is_verified = True
 
 
 
