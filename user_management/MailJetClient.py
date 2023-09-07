0176dc1a350a8691a4915777451b987fdba39557
Suja Shaj <sujashaj98@gmail.com>
2023-09-07 13:22:48 -0700

    modified MailJetClient.py

--- user_management/MailJetClient.py
+++ user_management/MailJetClient.py
@@ -2,9 +2,40 @@ from mailjet_rest import Client
 import os
 
 API_KEY = os.environ['MJ_APIKEY_PUBLIC']
-API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
+API_SECRET = os.environ['API_SECRET']
+SENDER_EMAIL = os.environ['SENDER_EMAIL']
+SENDER_NAME = os.environ['SENDER_NAME']
+
+
 
 class MailJetClient:
 
     def __init(self):
-        self.mailjet = Client(auth=(API_KEY, API_SECRET)
+        self.mailjet = Client(auth=(API_KEY, API_SECRET), version='v.31')
+
+
+    def send_email(self, receiver_email, receiver_name):
+        data = {
+            'Messages': [
+                {
+                    "From": {
+                        "Email": "{}".format(SENDER_EMAIL),
+                        "Name": "{}".format(SENDER_NAME)
+                    },
+                    "To": [
+                        {
+                            "Email": "{}".format(receiver_email),
+                            "Name": "{}".format(receiver_name)
+                        }
+                    ],
+                    "Subject": "Please verify your email address!",
+                    "TextPart": "Greetings from user management application!",
+                    "HTMLPart": "May the delivery force be with you!"
+                }
+            ]
+        }
+        result = self.mailjet.send.create(data=data)
+        return result.status_code
+
+
+
