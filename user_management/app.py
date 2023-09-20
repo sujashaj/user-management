40708c5eba06e1b27dede7b681e28d44a185d3df
Suja Shaj <sujashaj98@gmail.com>
2023-09-20 16:53:37 -0700

    Organize flask routes into class

--- user_management/app.py
+++ user_management/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from routes.routes import bp as routes_bp  # Import the Blueprint
+from routes.routes import bp as routes_bp# Import the Blueprint
+from routes.routes import Routes
+
 
 app = Flask(__name__)
 app.register_blueprint(routes_bp)  # Register the Blueprint
+routes = Routes()
+app.register_blueprint(routes.bp)
+
 
 
 if __name__ == "__main__":
--- user_management/routes.py
+++ user_management/routes.py
@@ -1,14 +1,25 @@
 from flask import Blueprint
 
 
-bp = Blueprint("routes", __name__)
+class Routes:
+    def __init(self):
+        self.bp = Blueprint("routes", __name__)
+        self.register_routes()
+
 
 @bp.route("/")
-def home():
+    def home(self):
     return "Welcome to Flask App!"
+    def about(self):
+        return "This is the about page."
+
+    def register_routes(self):
+        self.bp.add_url_rule("/", "home", self.home)
+        self.bp.add_url_rule("/about", "about", self.about)
 
 
 
 @bp.route("/about")
 def about()
      return "This is the about page."
+
