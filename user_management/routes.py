8fb2e7cb8da44acecfc9141740853292a6bebedb
Suja Shaj <sujashaj98@gmail.com>
2023-09-21 19:51:02 -0700

    Added user registration REST endpoint

--- user_management/routes.py
+++ user_management/routes.py
@@ -1,25 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from user_management.UserManager import UserManager
+
+DB_FILE = "user_database.db"
+
 
 
 class Routes:
     def __init(self):
         self.bp = Blueprint("routes", __name__)
         self.register_routes()
+        self.user_manager = UserManager(DB_FILE)
 
 
-@bp.route("/")
     def home(self):
-    return "Welcome to Flask App!"
-    def about(self):
-        return "This is the about page."
+        return "Welcome to the User Management Application"
+
+    def register_user(self):
+        data = request.get_json()
+        if data:
+            username = data.get("username")
+            email = data.get("email")
+            password = data.get("password")
+        response = self.user_manager.register_user(username, email, password)
+        return jsonify({"message": response}), 201
 
     def register_routes(self):
         self.bp.add_url_rule("/", "home", self.home)
-        self.bp.add_url_rule("/about", "about", self.about)
+        self.bp.add_url_rule("/register_user", "register_user", self.register_user, methods=["POST"])
 
 
 
-@bp.route("/about")
-def about()
-     return "This is the about page."
 
